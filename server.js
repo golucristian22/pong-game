065030f19fc74ae7249475192c6292735e6ac138
G.Cristian <golu.cristian22@gmail.com>
2023-04-29 12:32:39 +0300

    feat(server.js & script.js): now 2 players can connect to the same game

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ const PORT = 3000;
 server.listen(PORT);
 console.log(`Listening on port ${PORT}`);
 
+let readyPlayersCounter = 0;
+
 io.on("connection", (socket) => {
-  console.log("A user has connected!");
+  console.log("A user has connected!", socket.id);
+
+  socket.on("ready", () => {
+    console.log(`Player ${socket.id} is ready!`);
+    readyPlayersCounter++;
+  })
+
+  if(readyPlayersCounter === 2) {
+    io.emit("startGame", socket.id);
+  }
 });
